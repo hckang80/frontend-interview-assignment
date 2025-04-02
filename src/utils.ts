d9ece271af04b8dc1f2890ad16d66d021b6b1c6f
hckang80 <hckang80@gmail.com>
2025-04-02 22:48:04 +0900

    feat(utils): pdf 페이지수를 알기 위해 함수의 관심사 분리

--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,49 @@ import workerSrc from 'pdfjs-dist/build/pdf.worker?url';
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = workerSrc;
 
+export const loadPdf = async (
+  file: File
+): Promise<{
+  pdf: pdfjsLib.PDFDocumentProxy;
+  totalPages: number;
+}> => {
+  const pdfUrl = URL.createObjectURL(file);
+
+  try {
+    const pdf = await pdfjsLib.getDocument(pdfUrl).promise;
+    return {
+      pdf,
+      totalPages: pdf.numPages
+    };
+  } catch (error) {
+    console.error('Error loading PDF:', error);
+    throw new Error('Failed to load PDF file.');
+  }
+};
+
+export const getImageByPdf = async (
+  pdf: pdfjsLib.PDFDocumentProxy,
+  pageNumber = 1
+): Promise<string> => {
+  try {
+    const page = await pdf.getPage(pageNumber);
+    const viewport = page.getViewport({ scale: 1 });
+
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+
+    canvas.width = viewport.width;
+    canvas.height = viewport.height;
+
+    await page.render({ canvasContext: context!, viewport }).promise;
+
+    return canvas.toDataURL('image/png');
+  } catch (error) {
+    console.error(`Error rendering page ${pageNumber}:`, error);
+    throw new Error(`Failed to render page ${pageNumber}.`);
+  }
+};
+
 export const pdfFileToImage = async (
   file: File
 ): Promise<{
