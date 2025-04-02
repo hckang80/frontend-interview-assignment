d59db05f179e9f65daf56fb854dde0e1d3c980bd
hckang80 <hckang80@gmail.com>
2025-04-02 23:42:17 +0900

    feat: pdf를 리스트로 출력하기

--- src/C.tsx
+++ src/C.tsx
@@ -6,29 +6,34 @@ import { getImageByPdf, loadPdf } from './utils';
 
 const C = () => {
   const { file } = useStore();
-  const [fileImage, setFileImage] = useState<string | null>(null);
+  const [fileImages, setFileImages] = useState<string[]>([]);
 
   useEffect(() => {
     if (!file) return;
 
     (async () => {
       const { totalPages, pdf } = await loadPdf(file);
-      console.log({ totalPages });
-      setFileImage((await getImageByPdf(pdf)) ?? '');
+
+      const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
+      const loadedImages = await Promise.all(
+        pageNumbers.map((pageNumber) => getImageByPdf(pdf, pageNumber))
+      );
+
+      setFileImages(loadedImages);
     })();
   }, [file]);
 
   return (
     <div className="C">
       <div className="top">
-        {fileImage && (
-          <div>
+        {fileImages.map((image, index) => (
+          <div key={index}>
             <div className="image">
-              <img src={fileImage} />
+              <img src={image} alt="" />
             </div>
-            <div className="imageIndex">1</div>
+            <div className="imageIndex">{index + 1}</div>
           </div>
-        )}
+        ))}
       </div>
     </div>
   );
