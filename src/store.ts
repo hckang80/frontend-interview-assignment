83ae0b9edc1dadb747e4c028af6b5ea2d9101016
hckang80 <hckang80@gmail.com>
2025-04-03 00:10:19 +0900

    feat(store/pdf): 선택한 페이지 index 저장 함수 추가

--- src/store.ts
+++ src/store.ts
@@ -3,9 +3,13 @@ import { create } from 'zustand';
 type Store = {
   file: File | null;
   setFile: (file: File | null) => void;
+  selectedPageIndex: number;
+  setSelectedPageIndex: (index: number) => void;
 };
 
 export const useStore = create<Store>((set) => ({
   file: null,
-  setFile: (file: File | null) => set({ file })
+  setFile: (file: File | null) => set({ file }),
+  selectedPageIndex: 1,
+  setSelectedPageIndex: (index: number) => set({ selectedPageIndex: index })
 }));
