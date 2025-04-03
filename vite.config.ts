41e594caf229509b3c9c7a212f4368717cbc74fb
hckang80 <hckang80@gmail.com>
2025-04-03 18:28:32 +0900

    chore(vanilla-extract): 스타일 패키지 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vanillaExtractPlugin()],
   resolve: {
     alias: [{ find: '@', replacement: resolve(__dirname, 'src') }]
   }
