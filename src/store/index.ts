84223b3cb4efbb2628975a826cd3c841efac997c
hckang80 <hckang80@gmail.com>
2025-04-04 00:29:46 +0900

    feat: 미리보기 전용 스토어 함수 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,10 +3,13 @@ import { create } from 'zustand';
 type Store = {
   originFile: File | null;
   setOriginFile: (file: File | null) => void;
+  signedPreviewFile: File | null;
+  setSignedPreviewFile: (file: File | null) => void;
   signedFile: File | null;
   setSignedFile: (file: File | null) => void;
   selectedPageIndex: number;
   setSelectedPageIndex: (index: number) => void;
+  previewFile: () => File | null;
   printedFile: () => File | null;
   resetFile: () => void;
 };
@@ -14,18 +17,25 @@ type Store = {
 export const useStore = create<Store>((set, get) => ({
   originFile: null,
   setOriginFile: (file: File | null) => set({ originFile: file }),
+  signedPreviewFile: null,
+  setSignedPreviewFile: (file: File | null) => set({ signedPreviewFile: file }),
   signedFile: null,
   setSignedFile: (file: File | null) => set({ signedFile: file }),
   selectedPageIndex: 1,
   setSelectedPageIndex: (index: number) => set({ selectedPageIndex: index }),
+  previewFile: () => {
+    const { signedPreviewFile, originFile } = get();
+    return signedPreviewFile || originFile;
+  },
   printedFile: () => {
     const { signedFile, originFile } = get();
     return signedFile || originFile;
   },
   resetFile: () => {
-    const { setSignedFile, setOriginFile, setSelectedPageIndex } = get();
-    setSignedFile(null);
+    const { setOriginFile, setSignedFile, setSignedPreviewFile, setSelectedPageIndex } = get();
     setOriginFile(null);
+    setSignedPreviewFile(null);
+    setSignedFile(null);
     setSelectedPageIndex(1);
   }
 }));
